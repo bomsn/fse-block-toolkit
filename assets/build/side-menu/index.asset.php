7335606f442955c500c1b9dc7186ff8994d12971
bomsn <bomsn@live.fr>
2025-03-07 19:59:59 +0000

    initial release

--- assets/build/side-menu/index.asset.php
+++ assets/build/side-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n'), 'version' => '3beed2ae403b641c7bbf');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n'), 'version' => '0605d3a0a9add10e5442');
